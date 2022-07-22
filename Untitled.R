5b32b7d80b2bddf7c5500bfe560903438b362591
danicamalhotra <108920896+danicamalhotra@users.noreply.github.com>
2022-07-23 00:13:35 +0630

    completed task 1
    sum of two numbers

---
+++ Untitled.R
@@ -0,0 +1,7 @@
+sum(1,3)
+so whatever u write and update here will show up in ur github desktop
+
+u see that ? yes
+and then how do i put it on the website?
+  
+  Save this file -> go to github desktop ->
